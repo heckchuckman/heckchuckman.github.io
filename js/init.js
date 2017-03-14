ecf4268d0ba175ccc9d3f6d7bde5c7559da63931
heckchuckman <chuck.heckman@gmail.com>
2017-03-14 10:54:27 -0400

    just the resume for now.

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+        menuWidth: 240,
+    });
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
